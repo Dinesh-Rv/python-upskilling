5309bb3a3a35759385fcc0905f35dff08e0c18b9
Dineshkumar Ravikumar <dineshkumar.ravikumar@ideas2it.com>
2026-03-18 19:30:04 +0530

    Day 1 exercise

---
+++ week1.py
@@ -0,0 +1,13 @@
+# Day 1
+
+full_name = "Dineshkumar R"
+age = 24
+salary = 39.99
+is_employed = True
+
+print("Name", full_name , "type", type(full_name))
+print("Age", age, "type", type(age))
+print("Salary", salary, "type", type(salary))
+print("Employed?", is_employed , "type", type(is_employed))
+
+print(f"Name: {full_name} | type: {type(full_name)}")
